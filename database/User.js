49a9d29587730cdcc08c979a2f75f9bfe3789eed
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 18:49:50 +0600

    Update User.js

--- database/User.js
+++ database/User.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  user_id: { type: String, index: true },
+  isAdmin: { type: Boolean, default: false },
+  isWhitelist: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model("User", UserSchema);
