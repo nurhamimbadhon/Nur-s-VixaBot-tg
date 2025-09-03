a957ae3eb43e4859a65567a56a804d7b1b1bf885
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 18:32:52 +0600

    Update generate.py

--- session/generate.py
+++ session/generate.py
@@ -1,17 +1,14 @@
 from telethon import TelegramClient
 import os
 import asyncio
-
-# Ask for API ID & HASH
 API_ID = int(input("🔑 Enter your API ID: ").strip())
 API_HASH = input("🔑 Enter your API HASH: ").strip()
 
-# Session file in current folder
 SESSION_PATH = os.path.join(os.getcwd(), "main.session")
 
 async def main():
     client = TelegramClient(SESSION_PATH, API_ID, API_HASH)
-    await client.start()  # prompts for phone/code/2FA
+    await client.start() 
     print("✅ Logged in! Session saved at:", SESSION_PATH)
     await client.disconnect()
 
