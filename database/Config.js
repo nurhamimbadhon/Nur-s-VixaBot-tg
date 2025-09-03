5965093491c194890ed4a4d166940c514116da4d
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 18:48:09 +0600

    Update Config.js

--- database/Config.js
+++ database/Config.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
 
+const ConfigSchema = new mongoose.Schema({
+  prefix: { type: String, default: "!" },
+  selfListen: { type: Boolean, default: true },
+  adminOnly: { type: Boolean, default: false },
+  ownerOnly: { type: Boolean, default: false },
+  groupMode: { type: Boolean, default: true },
+  inboxMode: { type: Boolean, default: true },
+  admins: { type: [String], default: [] },
+  owners: { type: [String], default: [] },
+  whitelist: { type: [String], default: [] }
+});
+
+module.exports = mongoose.model("Config", ConfigSchema);
