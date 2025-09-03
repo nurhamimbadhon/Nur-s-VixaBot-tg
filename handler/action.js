e8ec4e7ad4825ee8ec00f8d278764e28ecbc46fc
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 18:59:09 +0600

    Update action.js

--- handler/action.js
+++ handler/action.js
@@ -1,1 +1,13 @@
+module.exports = async function actionHandler({ reaction, message, senderId, client }) {
+  try {
+    const chatId = message.chatId || message.peerId;
+    console.log(`User ${senderId} reacted with ${reaction} on chat ${chatId}`);
 
+    // Example: TCA/setReaction call
+    // const setReaction = require("../TCA/setReaction");
+    // await setReaction(client, chatId, message.id, "🙄");
+
+  } catch (err) {
+    console.error("❌ ActionHandler Error:", err);
+  }
+};
