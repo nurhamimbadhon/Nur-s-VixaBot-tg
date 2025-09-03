920e5b8805c5e4996abcfa9c82d0de6336c36a51
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 19:01:10 +0600

    Update setReaction.js

--- TCA/setReaction.js
+++ TCA/setReaction.js
@@ -1,1 +1,20 @@
+const { Api } = require("telegram");
 
+module.exports = async function setReaction(client, peer, msgId, emoji) {
+  try {
+    await client.invoke(
+      new Api.messages.SendReaction({
+        peer,
+        msgId: Number(msgId),
+        reaction: [
+          new Api.ReactionEmoji({ emoticon: emoji })
+        ],
+        addToRecent: true
+      })
+    );
+    return true;
+  } catch (err) {
+    console.error("setReaction error:", err);
+    throw err;
+  }
+};
