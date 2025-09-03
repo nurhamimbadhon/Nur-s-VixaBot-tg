74841dd4b780357c4129f2b9f4eb8b476eef4e46
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 19:01:42 +0600

    Update commandLoader.js

--- utils/commandLoader.js
+++ utils/commandLoader.js
@@ -1,1 +1,14 @@
+const fs = require("fs");
+const path = require("path");
 
+module.exports = function loadCommands() {
+  const cmds = new Map();
+  const base = path.join(__dirname, "../script/cmd");
+  const files = fs.readdirSync(base).filter(f => f.endsWith(".js"));
+  for (const file of files) {
+    const c = require(path.join(base, file));
+    cmds.set(c.config.name, c);
+  }
+  console.log("🧩 Commands loaded:", [...cmds.keys()].join(", "));
+  return cmds;
+};
