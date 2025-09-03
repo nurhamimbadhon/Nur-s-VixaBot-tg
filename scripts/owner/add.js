b0d23a61f4a59962293c4db15d253003c8cdc200
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-09-03 21:38:08 +0600

    Update add.js

--- scripts/owner/add.js
+++ scripts/owner/add.js
@@ -1,1 +1,22 @@
+const { addChat, addUser } = require('../../utils/dbHelpers');
 
+module.exports = {
+    command: 'add',
+    description: 'Add chat/user to allowed list',
+    require: 2, // Owner only
+    async run(client, message, args) {
+        const replyTo = await message.getReplyMessage();
+        
+        if (replyTo) {
+            // Add user
+            const targetUser = replyTo.senderId;
+            await addUser(targetUser, 1, message.senderId);
+            await message.edit({ text: `User ${targetUser} added as admin` });
+        } else {
+            // Add chat
+            const chatId = message.chatId;
+            await addChat(chatId, message.senderId);
+            await message.edit({ text: `Chat ${chatId} added to allowed list` });
+        }
+    }
+};
